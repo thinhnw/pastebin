afdc18c2982068337c210baf5f818ee60f760012
thinhnw <thinhnv0412@gmail.com>
2024-12-28 10:10:45 +0000

    Fix bugs

--- app/controllers/pastes_controller.rb
+++ app/controllers/pastes_controller.rb
@@ -82,6 +82,6 @@ class PastesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def paste_params
-      params.expect(paste: [ :title, :slug, :private, :content ])
+      params.expect(paste: [ :title, :slug, :private, :content, :language ])
     end
 end
