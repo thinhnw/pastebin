8a6d68b4db5033ce40a5b55d7442d6116b56f975
thinhnw <thinhnv0412@gmail.com>
2024-12-28 09:36:10 +0000

    Add syntax highlighting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  document.querySelectorAll("pre code").forEach((block) => {
+    hljs.highlightElement(block);
+  });
+})
