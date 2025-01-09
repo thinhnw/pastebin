4ccc97f95e1a37342de8feb62bdc2a99dfdbf1e5
thinhnw <thinhnv0412@gmail.com>
2025-01-09 13:51:08 +0000

    Guard pastes routes

--- app/controllers/pastes_controller.rb
+++ app/controllers/pastes_controller.rb
@@ -1,13 +1,10 @@
 class PastesController < ApplicationController
-  before_action :set_paste, only: %i[ show edit update destroy ]
+  before_action :set_paste, only: %i[ destroy ]
+  before_action :authorize_paste, only: %i[ show_by_slug raw destroy ]
 
   # GET /pastes or /pastes.json
   def index
     @pastes = Paste.all
-  end
-
-  # GET /pastes/1 or /pastes/1.json
-  def show
   end
 
   def show_by_slug
@@ -24,10 +21,6 @@ class PastesController < ApplicationController
   # GET /pastes/new
   def new
     @paste = Paste.new
-  end
-
-  # GET /pastes/1/edit
-  def edit
   end
 
   # POST /pastes or /pastes.json
@@ -51,18 +44,6 @@ class PastesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pastes/1 or /pastes/1.json
-  def update
-    respond_to do |format|
-      if @paste.update(paste_params)
-        format.html { redirect_to @paste, notice: "Paste was successfully updated." }
-        format.json { render :show, status: :ok, location: @paste }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @paste.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /pastes/1 or /pastes/1.json
   def destroy
@@ -83,5 +64,12 @@ class PastesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def paste_params
       params.expect(paste: [ :title, :slug, :private, :content, :language ])
+    end
+
+    def authorize_paste
+      paste = Paste.find_by(slug: params[:slug])
+      if paste.private? && (!user_signed_in?)
+        redirect_to root_path
+      end
     end
 end
