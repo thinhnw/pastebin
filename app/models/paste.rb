3511fe9e8cb03c072e4df472860dbbd364aec0f1
thinhnw <thinhnv0412@gmail.com>
2025-01-10 13:28:50 +0000

    Add validation for file size

--- app/models/paste.rb
+++ app/models/paste.rb
@@ -2,13 +2,21 @@ class Paste < ApplicationRecord
   before_validation :generate_slug, on: :create
 
   has_one_attached :content_file
+  validate :content_file_size_validation
 
   belongs_to :user, optional: true
 
+  validates :title, presence: true
   validates :slug, presence: true, uniqueness: true
 
   def to_param
     slug
+  end
+
+  def content_file_size_validation
+    if content_file.attached? && content_file.blob.byte_size > 1.kilobytes
+      errors.add(:content, "should be less than 1KB")
+    end
   end
 
   private
