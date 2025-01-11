e812909a5c463e1daa7e39e1b4f6e7c2f511c0f8
thinhnw <thinhnv0412@gmail.com>
2025-01-11 09:21:57 +0000

    Add Paste validation tests

--- app/models/paste.rb
+++ app/models/paste.rb
@@ -1,9 +1,10 @@
 class Paste < ApplicationRecord
-  before_validation :generate_slug, on: :create
   has_one_attached :content_file
+
+  before_validation :generate_slug, on: :create
   belongs_to :user, optional: true
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
   validates :slug, presence: true, uniqueness: true
   validate :validate_file_attachment
 
@@ -20,7 +21,10 @@ class Paste < ApplicationRecord
   end
 
   def validate_file_attachment
-    return unless content_file.attached?
+    if !content_file.attached?
+      errors.add(:content_file, "can't be blank")
+      return
+    end
     if content_file.blob.byte_size > 1.kilobytes
       errors.add(:content_file, "size must be less than 1KB")
     end
--- test/models/paste_test.rb
+++ test/models/paste_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 class PasteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @paste = pastes(:text_paste)
+  end
+
+  # Test Validations
+  test "should be valid with valid attributes" do
+    assert @paste.valid?
+  end
+
+  test "should require a title" do
+    @paste.title = nil
+    assert_not @paste.valid?
+    assert_includes @paste.errors[:title], "can't be blank"
+  end
+
+  test "should require a slug" do
+    @paste.slug = nil
+    assert_not @paste.valid?
+    assert_includes @paste.errors[:slug], "can't be blank"
+  end
+
+  test "slug should be unique" do
+    duplicate_paste = @paste.dup
+    @paste.save
+    assert_not duplicate_paste.valid?
+    assert_includes duplicate_paste.errors[:slug], "has already been taken"
+  end
+
+  test "should validate title length" do
+    @paste.title = "a" * 256
+    assert_not @paste.valid?
+    assert_includes @paste.errors[:title], "is too long (maximum is 255 characters)"
+  end
+
+  # Test Attachment
+  test "should require a content file" do
+    @paste.content_file = nil
+    assert_not @paste.valid?
+    assert_includes @paste.errors[:content_file], "can't be blank"
+  end
 end
